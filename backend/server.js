96f812ce912a14835b38983595a93f1fdd2385cc
Mustafa alhasan <mustafa.alhasan.2020@gmail.com>
2024-02-20 00:58:42 +0300

    reservation endpoint and .rest test

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,45 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const cors = require("cors");
+// Imports
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Import models
-const User = require("./models/user");
-const Reservation = require("./models/reservation");
-const Product = require("./models/product");
-const Stock = require("./models/stock");
-const Orders = require("./models/order");
+const User = require('./models/user');
+const Reservation = require('./models/reservation'); 
+const Product = require('./models/product');
+const Stock = require('./models/stock');
+const Order = require('./models/order');
 
+// Create Express app
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// Import & use reservations router
+const reservationsRouter = require('./controllers/reservations.router');
+reservationsRouter(app);
+
 // MongoDB Connection
-const uri =
-	"mongodb+srv://Mustafa:Mustafa00313@cluster0.9n4bpm2.mongodb.net/?retryWrites=true&w=majority";
+const uri = 
+  "mongodb+srv://Mustafa:Mustafa00313@cluster0.9n4bpm2.mongodb.net/?retryWrites=true&w=majority";
 
 async function connectToDatabase() {
-	try {
-		await mongoose.connect(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log("Connected to MongoDB Database");
-	} catch (error) {
-		console.error("Failed to connect to MongoDB Database:", error);
-	}
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to MongoDB Database");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB Database:", error);
+  }
 }
 
-// Start the server
+// Start server
+const port = process.env.PORT || 3000;
 app.listen(port, async () => {
-	await connectToDatabase();
-	console.log(`Server is running on port ${port}`);
+  await connectToDatabase();
+  console.log(`Server running on port ${port}`);
 });
