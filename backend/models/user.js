b284dbd032256fe7b9b3cdb9687a7ef8198016e8
NourHabra <NourHBSU@gmail.com>
2024-02-20 17:25:37 +0300

    Added Name to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },  
-    role: { type: String, enum: ['employee', 'manager'], required: true }
+	Name: { type: String, required: true, unique: true },
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	role: { type: String, enum: ["employee", "manager"], required: true },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
